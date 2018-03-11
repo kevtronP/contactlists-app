e84486b1b918870d849f831c48713d150be55dc2
Kevin Mercado <kmerc5187@gmail.com>
2018-03-11 10:39:34 -0500

    updated controller to be restful

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,29 +1,39 @@
 class V1::ContactsController < ApplicationController
-  def contacts
-    contact = Contact.first
-    render json: {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+
+  def index
+    contacts = Contact.all
+    render json: contacts.as_json
   end
-  def contact_two
-    contact = Contact.second
-    render json: {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+
+  def show
+    contact = Contact.find_by(id: params["id"])
+    render json: contact.as_json
   end
-  def contact_three
-    contact = Contact.third
-    render json: {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+
+  def create
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    contact.save
+    render json: contact.as_json
+  end
+
+  def update
+    contact = Contact.find_by(id: params["id"])
+    contact.first_name = params[:first_name] || contact.first_name
+    contact.last_name = params[:last_name] || contact.last_name
+    contact.email = params[:email] || contact.email
+    contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.save
+    render json: contact.as_json
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params["id"])
+    contact.destroy
+    render json: {message: "Contact has been deleted."}
   end
 end
